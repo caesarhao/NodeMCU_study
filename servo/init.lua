89453717e62cddf59d38f69df7757775d171f771
caesarhao <caesarhao@gmail.com>
2017-08-07 23:09:28 +0200

    servo 50 Hz

--- servo/init.lua
+++ servo/init.lua
@@ -1,31 +1,46 @@
 #! /usr/bin/lua
 -- demo to control servo SG90 using pwm and tmr
 local servo_p = 1 -- control with D1/GPIO5
-local duty = 0
-local duties = {27, 71, 123}
-local index = 1
+local duty = 0 
+--local duties = {27, 71, 123}
+local pulse_min = 1.0 -- 1.0 ms for 0
+local pulse_max = 2.0 -- 2.0 ms for 180
+local freq = 50 -- Hz 
+local period = 1000/freq -- ms
+local num = 10
+local pulse_delta = (pulse_max-pulse_min)/num
+local pulse = pulse_min
+local index = 1 
 local tmr0 = 0
 local ledPin = 4
-local ledState = 0
-
+local ledState = 1 
+ 
 gpio.mode(ledPin,gpio.OUTPUT)
--- Use 400 Hz to control servo
-pwm.setup(servo_p, 400, duty*1023/1000)
+-- Use 400 Hz to control servo, period is 2.5 ms
+duty = pulse/period;
+pwm.setup(servo_p, freq, duty*1023/1000)
 pwm.start(servo_p)
-
+ 
 function updateDuty()
-    ledState = 1 - ledState;
+    
     gpio.write(ledPin, ledState)
-    duty = duty + 10
-    if duty > 100 then
-        duty = 0
+    duty = 100*pulse/period;
+    print("current duty: " .. duty .. "%")
+    pwm.setduty(servo_p, math.floor(1024*duty/100))
+    if ledState > 0 then
+        pulse = pulse + pulse_delta
+        if pulse > pulse_max then
+            pulse = pulse_max
+            ledState = 0
+        end
+    else
+        pulse = pulse - pulse_delta
+        if pulse < pulse_min then
+            pulse = pulse_min
+            ledState = 1
+        end
     end
-    print("current duty: " .. duties[index]*100/1023)
-    pwm.setduty(servo_p, duties[index])
-    index = index + 1
-    if index > 3 then
-        index = 1
-    end
+    
 end
 
-tmr.alarm(tmr0, 5000, tmr.ALARM_AUTO, updateDuty)
+tmr.alarm(tmr0, 500, tmr.ALARM_AUTO, updateDuty)
