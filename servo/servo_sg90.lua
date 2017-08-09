dab9aac3e951b0bdb53dc3714802ca4e6c69daef
Hao ZHANG <caesarhao@126.com>
2017-08-09 18:25:52 +0200

    Update servo_sg90.lua

--- servo/servo_sg90.lua
+++ servo/servo_sg90.lua
@@ -1,7 +1,28 @@
 local servo_sg90 = {}
-function servo_sg90.foo()
-    print("Hello World!")
+
+local servo_sg90.Angle_AX =     { 0, 30, 60, 90, 120, 150, 180}
+local servo_sg90.Duty1023_Crv = {27, 43, 58, 71,  88, 105, 123}
+local servo_sg90.size = 0
+
+function servo_sg90.init()
+    servo_sg90.size = table.getn(servo_sg90.Angle_AX)
+end
+
+function servo_sg90.linearInterpol(angle)
+	local ret = 0
+	local i = 1
+	if angle <= servo_sg90.Angle_AX[1] then
+		ret = servo_sg90.Angle_AX[1]
+	elseif angle >= servo_sg90.Angle_AX[servo_sg90.size] then
+		ret = servo_sg90.Angle_AX[servo_sg90.size]
+	return ret
+	else
+		while angle < servo_sg90.Angle_AX[i]
+			i = i + 1
+		end
+		ret = (servo_sg90.Duty1023_Crv[i-1]*(angle-servo_sg90.Angle_AX[i-1]) + servo_sg90.Duty1023_Crv[i]*(servo_sg90.Angle_AX[i]-angle))/(servo_sg90.Angle_AX[i]-servo_sg90.Angle_AX[i-1]);
+	end
+	return ret
 end
 
 return servo_sg90
-
