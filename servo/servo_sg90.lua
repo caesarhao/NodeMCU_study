df4a22adfb9c1ead1a97b40488a3b3a18781d39a
Hao ZHANG <caesarhao@126.com>
2017-08-14 15:45:11 +0200

    Update servo_sg90.lua

--- servo/servo_sg90.lua
+++ servo/servo_sg90.lua
@@ -1,14 +1,14 @@
 local sg90_crv = require 'sg90_crv'
 local servo_sg90 = {}
-local servo_sg90.servo_p = 1
-local servo_sg90.angle_C = 90
+servo_sg90.servo_p = 1
+servo_sg90.angle_C = 90
 
-local function servo_sg90.servo_sg90_init_proc()
+function servo_sg90.servo_sg90_init_proc()
   pwm.setup(servo_sg90.servo_p, 50, sg90_crv.linearInterpol(90))
   pwm.start(servo_sg90.servo_p)
 end
 
-local function servo_sg90.servo_sg90_1000ms_proc()
+function servo_sg90.servo_sg90_1000ms_proc()
   pwm.setduty(servo_sg90.servo_p, sg90_crv.linearInterpol(servo_sg90.angle_C))
 end
 
